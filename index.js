77e3756e0aa0a0fcc93b483299b7180ff97da8ba
arm1xd <69009433+arm1xd@users.noreply.github.com>
2021-02-10 17:51:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// function num(a,b,c){
+//     if(c === 0){
+//         console.log('ERROR');
+//         return; 
+//     } else{
+
+//         return (a-b)/c;
+
+//     }
+// }
+
+
+// alert(num(5,8,1))
+
+//let arr= ['jojo' ,'bobo' , 'fofo' , 'dodo' , 'soso' , 'hoho' , 'roro']
+
+let index = 3;
+
+function month(i){
+    let array = ['jojo' ,'bobo' , 'fofo' , 'dodo' , 'soso' , 'hoho' , 'roro']
+    return array[i]
+}
+
+
+alert(month(index))
+
